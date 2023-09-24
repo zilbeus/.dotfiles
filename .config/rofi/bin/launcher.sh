8dd22cd7f48423e8b82c5b550b3d7d284a010dc4
zilq <118920399+z1lq@users.noreply.github.com>
2023-09-24 20:02:33 +0300

    Update rofi config

---
+++ .config/rofi/bin/launcher.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+FILE="$HOME/.config/rofi/styles/colors.rasi"
+
+rofi -no-config -monitor primary -no-lazy-grab -show drun -modi drun -theme ~/.config/rofi/styles/launcher.rasi
