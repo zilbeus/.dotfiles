fcd43df46b725a8e802389de7d2bdf9fb5effdbc
zilbeus <118920399+zilbeus@users.noreply.github.com>
2024-07-28 14:22:35 +0300

    add wezterm config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,38 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config.color_scheme = "zenbones_dark"
+config.enable_tab_bar = false
+config.window_decorations = "TITLE | RESIZE"
+config.enable_wayland = false
+config.font = wezterm.font({ family = "JetBrainsMono Nerd Font Mono", weight = "Bold" })
+config.font_size = 14.0
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
+config.colors = {
+	background = "#101010",
+	foreground = "#D8DEE9",
+	-- ansi = {
+	-- 	"#101010",
+	-- 	"#eee8d5",
+	-- 	"#2e2e2e",
+	-- 	"#859ba2",
+	-- 	"#424242",
+	-- 	"#DFDFDA",
+	-- 	"#c7cdd8",
+	-- 	"#DAD4C3",
+	-- },
+	-- brights = {
+	-- 	"#1f1f1f",
+	-- 	"#B8B7B1",
+	-- 	"#383838",
+	-- 	"#7b9198",
+	-- 	"#bfc5d0",
+	-- 	"#ced4df",
+	-- 	"#ced4df",
+	-- 	"#ced4df",
+	-- },
+}
+
+return config
