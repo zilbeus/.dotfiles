5b73dfab3b7b43ac376473051299b9afba4b0b32
zilbeus <118920399+zilbeus@users.noreply.github.com>
2024-08-11 18:50:48 +0300

    update keymaps

--- nvim/.config/nvim/lua/zq/keymaps/init.lua
+++ nvim/.config/nvim/lua/zq/keymaps/init.lua
@@ -41,4 +41,10 @@ vim.keymap.set("n", "<leader>gr", require("gitsigns").reset_hunk, { desc = "[R]e
 vim.keymap.set("n", "<leader>ghn", require("gitsigns").next_hunk, { desc = "[N]ext [G]it [H]unk" })
 vim.keymap.set("n", "<leader>ghp", require("gitsigns").prev_hunk, { desc = "[P]revious [G]it [H]unk" })
 
+-- BUFFER NAVIGATION
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+
 require("zq.keymaps.lsp")
