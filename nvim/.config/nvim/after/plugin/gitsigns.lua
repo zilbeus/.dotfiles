95f0140340766161293d8b7bcb3f28aa1cb32858
zilbeus <118920399+zilbeus@users.noreply.github.com>
2024-08-17 15:08:42 +0300

    modify gitsigns settings

--- nvim/.config/nvim/after/plugin/gitsigns.lua
+++ nvim/.config/nvim/after/plugin/gitsigns.lua
@@ -1,10 +1,10 @@
-require('gitsigns').setup {
-  signs = {
-    add = { text = '▎' },
-    change = { text = '▎' },
-    untracked = { text = '▎' },
-    delete = { text = '▶' },
-    topdelete = { text = '▶' },
-    changedelete = { text = '▶' },
-  }
-}
+require("gitsigns").setup({
+	signs = {
+		add = { text = "▌" },
+		change = { text = "▌" },
+		untracked = { text = "▌" },
+		delete = { text = "▌" },
+		topdelete = { text = "▶" },
+		changedelete = { text = "▌" },
+	},
+})
