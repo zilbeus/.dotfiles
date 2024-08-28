0c9a6f43ff1224e5bda592335161889853b8282c
zilbeus <118920399+zilbeus@users.noreply.github.com>
2024-08-28 19:45:41 +0300

    add postgresql helper func

---
+++ nvim/.config/nvim/after/plugin/dadbod-ui.lua
@@ -0,0 +1,5 @@
+vim.g.db_ui_table_helpers f {
+	postgresql = {
+		Comments = 'select table_name, column_name, col_description((table_schema||\'.\'||table_name)::regclass::oid, ordinal_position) as column_comment from information_schema.columns where table_name\'{table}\' and table_schema=\'{schema}\''
+	}
+}
