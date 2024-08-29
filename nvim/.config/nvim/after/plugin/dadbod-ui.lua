2ea9e2c39d5e3251a43c5fdb0b51b67318d7338f
zilbeus <118920399+zilbeus@users.noreply.github.com>
2024-08-29 19:25:22 +0300

    fix syntax error

--- nvim/.config/nvim/after/plugin/dadbod-ui.lua
+++ nvim/.config/nvim/after/plugin/dadbod-ui.lua
@@ -1,5 +1,5 @@
-vim.g.db_ui_table_helpers f {
+vim.g.db_ui_table_helpers = {
 	postgresql = {
-		Comments = 'select table_name, column_name, col_description((table_schema||\'.\'||table_name)::regclass::oid, ordinal_position) as column_comment from information_schema.columns where table_name\'{table}\' and table_schema=\'{schema}\''
-	}
+		Comments = "select table_name, column_name, col_description((table_schema||'.'||table_name)::regclass::oid, ordinal_position) as column_comment from information_schema.columns where table_name'{table}' and table_schema='{schema}'",
+	},
 }
