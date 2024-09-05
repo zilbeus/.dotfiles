a3d7a14f017dbb2bd01bbdde31e3967c65473999
zilbeus <118920399+zilbeus@users.noreply.github.com>
2024-09-05 22:09:50 +0300

    modify telescope path display

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -4,6 +4,10 @@ require("telescope").setup({
 		layout_config = {
 			vertical = { width = 0.60, mirror = true, prompt_position = "top", height = 0.5 },
 		},
+		path_display = function(opts, path)
+			local tail = require("telescope.utils").path_tail(path)
+			return string.format("%s -- (%s)", tail, path)
+		end,
 		-- borderchars = {
 		-- 	"─",
 		-- 	"│",
