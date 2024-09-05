838f8209fb8db9adefce151beab70643458f77ec
zilbeus <118920399+zilbeus@users.noreply.github.com>
2024-09-05 20:55:45 +0300

    modify diffview diff colors

--- nvim/.config/nvim/after/plugin/diffview.lua
+++ nvim/.config/nvim/after/plugin/diffview.lua
@@ -3,6 +3,23 @@ require("diffview").setup({
 	use_icons = true,
 	enhanced_diff_hl = true,
 	watch_index = true,
+	hooks = {
+		diff_buf_win_enter = function(bufnr, winid, ctx)
+			if ctx.layout_name:match("^diff2") then
+				if ctx.symbol == "a" then
+					vim.opt_local.winhl = table.concat({
+						"DiffChange:DiffDelete",
+						"DiffText:DiffDeleteText",
+					}, ",")
+				elseif ctx.symbol == "b" then
+					vim.opt_local.winhl = table.concat({
+						"DiffChange:DiffAdd",
+						"DiffText:DiffAddText",
+					}, ",")
+				end
+			end
+		end,
+	},
 })
 
 vim.opt.fillchars = vim.opt.fillchars + "diff:╱"
--- nvim/.config/nvim/lua/zq/theme/integrations/diffview.lua
+++ nvim/.config/nvim/lua/zq/theme/integrations/diffview.lua
@@ -2,7 +2,9 @@ local colors = require("zq.theme.monochrome.palette")
 
 return {
 	DiffDelete = { bg = "#402424", fg = "NONE" },
+	DiffDeleteText = { bg = "#7f4747", fg = "NONE" },
 	DiffAdd = { bg = "#012011", fg = "NONE" },
+	DiffAddText = { bg = "#1C582E", fg = "NONE" },
 	DiffChange = { bg = "#091521", fg = "NONE" },
 	DiffText = { bg = "#25476A", fg = "NONE" },
 }
