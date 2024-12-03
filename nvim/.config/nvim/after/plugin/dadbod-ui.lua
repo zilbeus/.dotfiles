c016f7889c9f8db8b22b89746b5a036ea9b91992
zlq <zlq@ul005227.eu.tieto.com>
2024-12-03 21:23:49 +0200

    modify dadbod-ui automatic exec config

--- nvim/.config/nvim/after/plugin/dadbod-ui.lua
+++ nvim/.config/nvim/after/plugin/dadbod-ui.lua
@@ -3,3 +3,8 @@ vim.g.db_ui_table_helpers = {
 		Comments = "select table_name, column_name, col_description((table_schema||'.'||table_name)::regclass::oid, ordinal_position) as column_comment from information_schema.columns where table_name='{table}' and table_schema='{schema}'",
 	},
 }
+
+vim.g.db_ui_execute_on_save = 0
+vim.g.db_ui_auto_execute_table_helpers = 1
+
+vim.g.db_ui_winwidth = 50
