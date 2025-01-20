36a473f6af5d254318c13a050da72f3921aa23f0
zilbeus <118920399+zilbeus@users.noreply.github.com>
2025-01-20 22:14:18 +0200

    modify fzf-lua previewer title colors

--- nvim/.config/nvim/lua/zq/theme/integrations/fzf-lua.lua
+++ nvim/.config/nvim/lua/zq/theme/integrations/fzf-lua.lua
@@ -2,4 +2,5 @@ local colors = require("zq.theme.monochrome.palette")
 
 return {
 	FzfLuaBorderInvisible = { fg = colors.base00 },
+	FzfLuaPreviewTitle = { fg = colors.black, bg = colors.pmenu_bg },
 }
