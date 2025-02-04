27f429f7233abb2825f6fd5f0a48b5bb38b94c37
zilbeus <118920399+zilbeus@users.noreply.github.com>
2025-02-04 22:47:54 +0200

    fix fillchars

--- nvim/.config/nvim/after/plugin/misc.lua
+++ nvim/.config/nvim/after/plugin/misc.lua
@@ -1,4 +1,4 @@
 require("nvim-ts-autotag").setup()
 require("luasnip.loaders.from_vscode").lazy_load()
 
-vim.opt.fillchars = vim.opt.fillchars + "foldopen:,foldclose:"
+vim.opt.fillchars = vim.opt.fillchars + "foldclose:,foldopen:"
