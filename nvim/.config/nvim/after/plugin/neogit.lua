e9f15171a4d1bb57924f9ebcf5555ee4681695c7
zilbeus <118920399+zilbeus@users.noreply.github.com>
2025-02-04 22:14:29 +0200

    modify neogit config

--- nvim/.config/nvim/after/plugin/neogit.lua
+++ nvim/.config/nvim/after/plugin/neogit.lua
@@ -5,4 +5,10 @@ require("neogit").setup({
 		item = { "", "" },
 		section = { "", "" },
 	},
+	integrations = {
+		telescope = false,
+		diffview = true,
+		fzf_lua = true,
+		mini_pick = false,
+	},
 })
