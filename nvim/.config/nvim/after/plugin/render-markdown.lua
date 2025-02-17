1e4b86238f05c759976b842d736370421ad04b13
zilbeus <118920399+zilbeus@users.noreply.github.com>
2025-02-17 12:37:40 +0200

    add render-markdown config

--- nvim/.config/nvim/after/plugin/render-markdown.lua
+++ nvim/.config/nvim/after/plugin/render-markdown.lua
@@ -1,1 +1,14 @@
-require("render-markdown").setup({})
+require("render-markdown").setup({
+	heading = {
+		position = 'inline',
+		border = true,
+		border_virtual = true,
+		icons = { ' 󰎤  ', ' 󰎧 ', ' 󰎪 ', ' 󰎭 ', ' 󰎱 ', ' 󰎳 ' },
+	},
+	code = {
+		style = 'normal',
+		border = 'thick',
+		sign = true,
+		width = 'full',
+	},
+})
