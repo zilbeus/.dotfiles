f72150cdf81308c9546ccbcd413bacb3df1800eb
zilbeus <118920399+zilbeus@users.noreply.github.com>
2025-10-13 23:28:01 +0300

    modify nvim dashboard config

--- nvim/.config/nvim/lua/custom/plugins/snacks.lua
+++ nvim/.config/nvim/lua/custom/plugins/snacks.lua
@@ -17,25 +17,13 @@ return {
                                 ░                   
 					]],
 				keys = {
-					{ icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+					{ icon = " ", key = "f", desc = "Find File", action = "<leader>ff" },
 					{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-					{
-						icon = " ",
-						key = "g",
-						desc = "Find Text",
-						action = ":lua Snacks.dashboard.pick('live_grep')",
-					},
 					{
 						icon = " ",
 						key = "r",
 						desc = "Recent Files",
-						action = ":lua Snacks.dashboard.pick('oldfiles')",
-					},
-					{
-						icon = " ",
-						key = "c",
-						desc = "Config",
-						action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
+						action = "<leader>fr",
 					},
 					{ icon = " ", key = "s", desc = "Restore Session", action = "<leader>gs" },
 					{
