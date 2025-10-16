fbbd94310def452057f8a13a0d0fa2ce7fb96e53
zilbeus <118920399+zilbeus@users.noreply.github.com>
2025-10-16 11:36:04 +0300

    modify nvim keymaps

--- nvim/.config/nvim/lua/zq/keymaps/lsp.lua
+++ nvim/.config/nvim/lua/zq/keymaps/lsp.lua
@@ -8,36 +8,35 @@ end
 
 nmap("<leader>csr", vim.lsp.buf.rename, "rename symbol")
 
-nmap("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 vim.keymap.set(
 	"n",
 	"<leader>ci",
-	"<cmd>FzfLua lsp_implementations hls.border=FzfLuaBorderInvisible jump_to_single_result=true<cr>",
+	"<cmd>FzfLua lsp_implementations hls.border=FzfLuaBorderInvisible jump1=true<cr>",
 	{ desc = "implementations" }
 )
 vim.keymap.set(
 	"n",
 	"<leader>cdd",
-	"<cmd>FzfLua lsp_definitions hls.border=FzfLuaBorderInvisible jump_to_single_result=true<cr>",
+	"<cmd>FzfLua lsp_definitions hls.border=FzfLuaBorderInvisible jump1=true<cr>",
 	{ desc = "definitions" }
 )
 vim.keymap.set(
 	"n",
 	"gdd",
-	"<cmd>FzfLua lsp_definitions hls.border=FzfLuaBorderInvisible jump_to_single_result=true<cr>",
+	"<cmd>FzfLua lsp_definitions hls.border=FzfLuaBorderInvisible jump1=true<cr>",
 	{ desc = "definitions" }
 )
 vim.keymap.set("n", "gdD", "<cmd>Glance definitions<cr>", { desc = "Peek definitions" })
 vim.keymap.set(
 	"n",
 	"<leader>cdt",
-	"<cmd>FzfLua lsp_typedefs hls.border=FzfLuaBorderInvisible jump_to_single_result=true<cr>",
+	"<cmd>FzfLua lsp_typedefs hls.border=FzfLuaBorderInvisible jump1=true<cr>",
 	{ desc = "type definitions" }
 )
 vim.keymap.set(
 	"n",
 	"gdt",
-	"<cmd>FzfLua lsp_typedefs hls.border=FzfLuaBorderInvisible jump_to_single_result=true<cr>",
+	"<cmd>FzfLua lsp_typedefs hls.border=FzfLuaBorderInvisible jump1=true<cr>",
 	{ desc = "type definitions" }
 )
 vim.keymap.set("n", "gdT", "<cmd>Glance type_definitions<cr>", { desc = "Peek type definitions" })
@@ -58,7 +57,7 @@ vim.keymap.set(
 vim.keymap.set(
 	"n",
 	"<leader>cr",
-	"<cmd>FzfLua lsp_references hls.border=FzfLuaBorderInvisible jump_to_single_result=true<cr>",
+	"<cmd>FzfLua lsp_references hls.border=FzfLuaBorderInvisible jump1=true<cr>",
 	{ desc = "code references" }
 )
 vim.keymap.set(
@@ -67,6 +66,19 @@ vim.keymap.set(
 	"<cmd>FzfLua diagnostics_workspace hls.border=FzfLuaBorderInvisible<cr>",
 	{ desc = "search diagnostics" }
 )
+vim.keymap.set(
+	"n",
+	"<leader>cS",
+	"<cmd>FzfLua lsp_live_workspace_symbols hls.border=FzfLuaBorderInvisible<cr>",
+	{ desc = "workspace symbols" }
+)
+
+vim.keymap.set(
+	"n",
+	"gS",
+	"<cmd>FzfLua lsp_live_workspace_symbols hls.border=FzfLuaBorderInvisible<cr>",
+	{ desc = "workspace symbols" }
+)
 
 -- See `:help K` for why this keymap
 nmap("K", vim.lsp.buf.hover, "Hover Documentation")
@@ -74,11 +86,6 @@ nmap("<leader>ch", vim.lsp.buf.signature_help, "Signature Documentation")
 
 -- Lesser used LSP functionality
 nmap("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
-nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-nmap("<leader>wl", function()
-	print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-end, "[W]orkspace [L]ist Folders")
 
 -- Create a command `:Format` local to the LSP buffer
 vim.api.nvim_buf_create_user_command(0, "Format", function(_)
@@ -86,7 +93,6 @@ vim.api.nvim_buf_create_user_command(0, "Format", function(_)
 end, { desc = "Format current buffer with LSP" })
 
 nmap("<leader>bf", vim.lsp.buf.format, "[F]ormat buffer")
-nmap("<leader>ob", ":Telescope file_browser path=%:p:h select_buffer=true<CR>", "[O]pen File [B]rowser")
 nmap("<leader>hrr", ":Gitsigns reset_hunk<CR>", "[R]eset hunk")
 nmap("<leader>hrb", ":Gitsigns reset_buffer<CR>", "[R]eset buffer")
 nmap("<leader>hp", ":Gitsigns preview_hunk<CR>", "[P]review hunk")
