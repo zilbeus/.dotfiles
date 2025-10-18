a7e68c4a2c51be7eedd05f78e83efe2e653d10bf
zilbeus <118920399+zilbeus@users.noreply.github.com>
2025-10-18 13:05:16 +0300

    modify diffview config

--- nvim/.config/nvim/lua/zq/theme/integrations/diffview.lua
+++ nvim/.config/nvim/lua/zq/theme/integrations/diffview.lua
@@ -1,10 +1,10 @@
 local colors = require("zq.theme.monochrome.palette")
 
 return {
-	DiffDelete = { bg = "#402424", fg = "NONE" },
-	DiffDeleteText = { bg = "#7f4747", fg = "NONE" },
-	DiffAdd = { bg = "#012011", fg = "NONE" },
-	DiffAddText = { bg = "#1C582E", fg = "NONE" },
+	DiffDelete = { bg = "#25171B", fg = "NONE" },
+	DiffDeleteText = { bg = "#532426", fg = "NONE" },
+	DiffAdd = { bg = "#12261E", fg = "NONE" },
+	DiffAddText = { bg = "#1B4327", fg = "NONE" },
 	DiffChange = { bg = "#091521", fg = "NONE" },
 	DiffText = { bg = "#25476A", fg = "NONE" },
 }
