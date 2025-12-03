3325597dd5f6a420123e6249375fbe7d2e846b60
zilbeus <118920399+zilbeus@users.noreply.github.com>
2025-12-03 23:29:51 +0200

    modify tabby plugin config

--- nvim/.config/nvim/after/plugin/tabby.lua
+++ nvim/.config/nvim/after/plugin/tabby.lua
@@ -1,60 +1,65 @@
 local colors = require("zq.theme.monochrome.palette")
 
 local theme = {
-	active_tab = { fg = colors.base02, bg = colors.pmenu_bg },
-	active_tab_left_sep = { fg = colors.base02, bg = colors.pmenu_bg },
-	active_tab_right_sep = { fg = colors.base02, bg = colors.pmenu_bg },
-	inactive_tab = { fg = colors.base07, bg = colors.lightbg },
-	inactive_tab_left_sep = { fg = colors.base07, bg = colors.lightbg },
-	inactive_tab_right_sep = { fg = colors.base07, bg = colors.lightbg },
-	fill = { fg = colors.base07, bg = colors.base01 },
-	active_win = { fg = colors.base02, bg = colors.pmenu_bg },
-	active_win_left_sep = { fg = colors.base01, bg = colors.pmenu_bg },
-	active_win_right_sep = { bg = colors.pmenu_bg },
-	inactive_win = { fg = colors.base07, bg = colors.lightbg },
-	inactive_win_left_sep = { fg = colors.base01, bg = colors.lightbg },
-	inactive_win_right_sep = { bg = colors.lightbg },
+	active_tab = { fg = colors.base05, bg = colors.base01 },
+	active_tab_left_sep = { fg = colors.teal, bg = colors.teal },
+	active_tab_close_btn = { fg = colors.red, bg = colors.base01 },
+	active_tab_nr = { fg = colors.teal, bg = colors.base01 },
+	inactive_tab = { fg = colors.grey_fg2, bg = colors.base00 },
+	inactive_tab_left_sep = { fg = colors.base07, bg = colors.base00 },
+	inactive_tab_close_btn = { fg = colors.grey_fg2, bg = colors.base00 },
+	inactive_tab_nr = { fg = colors.grey_fg2, bg = colors.base00 },
+	fill = { fg = colors.base07, bg = colors.real_black },
 	placeholder = { fg = "transparent", bg = "transparent" },
 }
 
 require("tabby.tabline").set(function(line)
-	local cwd = " " .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t") .. " "
 	return {
-		{
-			{ cwd, hl = { fg = colors.base02, bg = colors.pmenu_bg } },
-			{ "", hl = { fg = colors.base01, bg = colors.pmenu_bg } },
-		},
 		line.tabs().foreach(function(tab)
 			local hl = tab.is_current() and theme.active_tab or theme.inactive_tab
-			local left_sep_hl = tab.is_current() and theme.active_tab_left_sep or theme.inactive_tab_left_sep
-			local right_sep_hl = tab.is_current() and theme.active_tab_right_sep or theme.inactive_tab_right_sep
-			local icon = tab.is_current() and " 󱗜 " or " 󱗝 "
+			local left_sep
+			local close_btn
+			local tab_nr
+			if tab.is_current() then
+				left_sep = line.sep("▎", theme.active_tab_left_sep, theme.active_tab)
+				close_btn = {
+					tab.close_btn(" "),
+					hl = theme.active_tab_close_btn,
+				}
+				tab_nr = {
+					"[",
+					{
+						tab.number(),
+						hl = theme.active_tab_nr,
+					},
+					"]",
+				}
+			else
+				left_sep = line.sep("▎", theme.inactive_tab_left_sep, theme.inactive_tab)
+				close_btn = {
+					tab.close_btn(" "),
+					hl = theme.inactive_tab_close_btn,
+				}
+				tab_nr = {
+					"[",
+					{
+						tab.number(),
+						hl = theme.inactive_tab_nr,
+					},
+					"]",
+				}
+			end
 
 			return {
-				line.sep("", left_sep_hl, theme.fill),
-				icon .. tab.number() .. " ",
-				line.sep("", theme.fill, right_sep_hl),
+				-- left_sep,
+				tab_nr,
+				tab.name(),
+				close_btn,
 				hl = hl,
+				margin = " ",
 			}
 		end),
 		line.spacer(),
-		line.wins_in_tab(line.api.get_current_tab()).foreach(function(win)
-			local hl = win.is_current() and theme.active_win or theme.inactive_win
-			local left_sep_hl = win.is_current() and theme.active_win_left_sep or theme.inactive_win_left_sep
-			local right_sep_hl = win.is_current() and theme.active_win_right_sep or theme.inactive_win_right_sep
-			local icon = win.is_current() and "  " or "  "
-
-			return {
-				line.sep("", theme.fill, left_sep_hl),
-				icon .. win.buf_name() .. " ",
-				line.sep("", right_sep_hl, theme.fill),
-				hl = hl,
-			}
-		end),
-		{
-			{ "", hl = { fg = colors.base01, bg = colors.lightbg } },
-			{ "   ", hl = { fg = colors.base07, bg = colors.lightbg } },
-		},
 		hl = theme.fill,
 	}
 end)
