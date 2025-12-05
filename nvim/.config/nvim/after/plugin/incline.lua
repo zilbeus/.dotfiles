6b9bcadcb801ff1c572ca6358ec56599dd6c1dc3
zilbeus <118920399+zilbeus@users.noreply.github.com>
2025-12-06 00:36:59 +0200

    disable incline

--- nvim/.config/nvim/after/plugin/incline.lua
+++ nvim/.config/nvim/after/plugin/incline.lua
@@ -1,46 +1,46 @@
-local c = require("zq.theme.monochrome.palette")
-
-require("incline").setup({
-	window = {
-		margin = {
-			vertical = 1,
-			horizontal = 1,
-		},
-		padding = 0,
-	},
-	hide = {
-		cursorline = false,
-		focused_win = true,
-		only_win = true,
-	},
-	render = function(props)
-		local fname = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-		local modified_glyph = {}
-		if vim.api.nvim_buf_get_option(props.buf, "modified") then
-			modified_glyph = vim.tbl_extend("force", { "[+] " }, {})
-		end
-		if props.focused == true then
-			return {
-				{
-					{ " " },
-					fname,
-					{ " " },
-					modified_glyph,
-					guifg = "#292929",
-					guibg = "#bfc5d0",
-				},
-			}
-		else
-			return {
-				{
-					{ " " },
-					fname,
-					{ " " },
-					modified_glyph,
-					guifg = c.white,
-					guibg = c.darker_black,
-				},
-			}
-		end
-	end,
-})
+-- local c = require("zq.theme.monochrome.palette")
+--
+-- require("incline").setup({
+-- 	window = {
+-- 		margin = {
+-- 			vertical = 1,
+-- 			horizontal = 1,
+-- 		},
+-- 		padding = 0,
+-- 	},
+-- 	hide = {
+-- 		cursorline = false,
+-- 		focused_win = true,
+-- 		only_win = true,
+-- 	},
+-- 	render = function(props)
+-- 		local fname = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+-- 		local modified_glyph = {}
+-- 		if vim.api.nvim_buf_get_option(props.buf, "modified") then
+-- 			modified_glyph = vim.tbl_extend("force", { "[+] " }, {})
+-- 		end
+-- 		if props.focused == true then
+-- 			return {
+-- 				{
+-- 					{ " " },
+-- 					fname,
+-- 					{ " " },
+-- 					modified_glyph,
+-- 					guifg = "#292929",
+-- 					guibg = "#bfc5d0",
+-- 				},
+-- 			}
+-- 		else
+-- 			return {
+-- 				{
+-- 					{ " " },
+-- 					fname,
+-- 					{ " " },
+-- 					modified_glyph,
+-- 					guifg = c.white,
+-- 					guibg = c.darker_black,
+-- 				},
+-- 			}
+-- 		end
+-- 	end,
+-- })
