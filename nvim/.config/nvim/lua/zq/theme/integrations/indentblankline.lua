9ddd587fc7192422a1ef866503d64e34a127c8f7
zilbeus <118920399+zilbeus@users.noreply.github.com>
2025-12-17 15:05:49 +0200

    update indent-blankline config

--- nvim/.config/nvim/lua/zq/theme/integrations/indentblankline.lua
+++ nvim/.config/nvim/lua/zq/theme/integrations/indentblankline.lua
@@ -1,5 +1,6 @@
 local colors = require("zq.theme.monochrome.palette")
 
 return {
-	IndentScope = { fg = colors.indent_line },
+	IndentScope = { fg = colors.active_indent_line },
+	IblIndent = { fg = colors.indent_line },
 }
