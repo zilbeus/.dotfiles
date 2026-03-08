174f387c5c3c0ba13e18727301574dfb5b617c59
zilbeus <118920399+zilbeus@users.noreply.github.com>
2026-03-08 18:11:11 +0200

    add opencode.nvim plugin

--- nvim/.config/nvim/lua/custom/plugins/init.lua
+++ nvim/.config/nvim/lua/custom/plugins/init.lua
@@ -1,254 +1,289 @@
 return {
-	{
-		"folke/lazydev.nvim",
-		ft = "lua", -- only load on lua files
-		opts = {
-			library = {
-				-- See the configuration section for more details
-				-- Load luvit types when the `vim.uv` word is found
-				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
-			},
-		},
-	},
-	{
-		"kdheepak/lazygit.nvim",
-		lazy = true,
-		cmd = {
-			"LazyGit",
-			"LazyGitConfig",
-			"LazyGitCurrentFile",
-			"LazyGitFilter",
-			"LazyGitFilterCurrentFile",
-		},
-		-- optional for floating window border decoration
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		-- setting the keybinding for LazyGit with 'keys' is recommended in
-		-- order to load the plugin when the command is run for the first time
-		keys = {
-			{ "<leader>gl", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-		},
-	},
-	{
-		"karb94/neoscroll.nvim",
-		opts = {},
-	},
-	{
-		"folke/noice.nvim",
-		event = "VeryLazy",
-		dependencies = {
-			"MunifTanjim/nui.nvim",
-			"rcarriga/nvim-notify",
-		},
-	},
-	{
-		"akinsho/toggleterm.nvim",
-		config = true,
-	},
-	{
-		"nvim-neo-tree/neo-tree.nvim",
-		branch = "v3.x",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim",
-			"nvim-tree/nvim-web-devicons",
-		},
-	},
-	"luukvbaal/statuscol.nvim",
-	"tpope/vim-fugitive",
-	{
-		"LintaoAmons/bookmarks.nvim",
-		dependencies = {
-			{ "kkharji/sqlite.lua" },
-			{ "stevearc/dressing.nvim" },
-		},
-		config = function()
-			local opts = {}
-			require("bookmarks").setup(opts)
-		end,
-	},
-	{
-		"SmiteshP/nvim-navic",
-		dependencies = { "neovim/nvim-lspconfig" },
-	},
-	{
-		"ibhagwan/fzf-lua",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-	},
-	{
-		"saghen/blink.cmp",
-		version = "v1.8.0",
-		---@module 'blink.cmp'
-		---@type blink.cmp.Config
-		opts = {
-			fuzzy = { implementation = "rust" },
-			cmdline = {
-				keymap = { preset = "inherit" },
-				completion = {
-					menu = { auto_show = true },
-				},
-			},
-			keymap = { preset = "default" },
-			completion = {
-				documentation = {
-					window = {
-						border = "single",
-					},
-				},
-				menu = {
-					-- border = { "┌", "─", "┐", "│", "┘", "─", "└", "│" },
-					border = "single",
-					winhighlight = "Normal:Pmenu,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:Search",
-					draw = {
-						columns = { { "kind_icon" }, { "label", "label_description", gap = 1 }, { "kind" } },
-						components = {
-							label = {
-								width = { fill = true, max = 120 },
-							},
-							label_description = {
-								width = { fill = true, max = 120 },
-							},
-							kind = {
-								text = function(ctx)
-									return "[" .. ctx.kind .. "]"
-								end,
-							},
-						},
-					},
-				},
-			},
-			signature = {
-				window = {
-					border = "single",
-				},
-			},
-			appearance = {
-				use_nvim_cmp_as_default = true,
-				nerd_font_variant = "normal",
-			},
-			sources = {
-				default = { "lsp", "path", "snippets", "buffer", "dadbod", "lazydev" },
-				providers = {
-					lazydev = {
-						name = "LazyDev",
-						module = "lazydev.integrations.blink",
-						score_offset = 100,
-					},
-					dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
-				},
-				min_keyword_length = function(ctx)
-					return ctx.trigger.kind == "trigger_character" and 0 or 3
-				end,
-			},
-		},
-		opts_extend = { "sources.default" },
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = { "saghen/blink.cmp" },
+    {
+        "nickjvandyke/opencode.nvim",
+        version = "*", -- Latest stable release
+        config = function()
+            ---@type opencode.Opts
+            vim.g.opencode_opts = {
+                -- Your configuration, if any; goto definition on the type or field for details
+            }
 
-		-- example using `opts` for defining servers
-		opts = {
-			servers = {},
-		},
-		config = function(_, opts)
-			local lspconfig = require("lspconfig")
-			for server, config in pairs(opts.servers) do
-				-- passing config.capabilities to blink.cmp merges with the capabilities in your
-				-- `opts[server].capabilities, if you've defined it
-				config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-				lspconfig[server].setup(config)
-			end
-		end,
+            vim.o.autoread = true -- Required for `opts.events.reload`
 
-		-- example calling setup directly for each LSP
-		-- config = function()
-		-- 	local capabilities = require("blink.cmp").get_lsp_capabilities()
-		-- 	-- local lspconfig = require("lspconfig")
-		--
-		-- 	vim.lsp.config["lua-ls"].setup({ capabilities = capabilities })
-		-- end,
-	},
-	{
-		"stevearc/conform.nvim",
-		opts = {},
-	},
-	{
+            -- Recommended/example keymaps
+            vim.keymap.set({ "n", "x" }, "<leader>la",
+                function() require("opencode").ask("@this: ", { submit = true }) end,
+                { desc = "Ask opencode…" })
+            vim.keymap.set({ "n", "x" }, "<leader>ls", function() require("opencode").select() end,
+                { desc = "Execute opencode action…" })
+            vim.keymap.set({ "n" }, "<leader>ll", function() require("opencode").toggle() end,
+                { desc = "Toggle opencode" })
 
-		"L3MON4D3/LuaSnip",
-		dependencies = { "rafamadriz/friendly-snippets" },
-	},
-	{
-		"folke/which-key.nvim",
-	},
+            vim.keymap.set({ "n", "x" }, "<leader>lgo", function() return require("opencode").operator("@this ") end,
+                { desc = "Add range to opencode", expr = true })
+            vim.keymap.set("n", "<leader>lgoo", function() return require("opencode").operator("@this ") .. "_" end,
+                { desc = "Add line to opencode", expr = true })
 
-	{
-		"windwp/nvim-autopairs",
-		event = "InsertEnter",
-		opts = {},
-	},
-	{
-		"windwp/nvim-ts-autotag",
-		config = function() end,
-	},
-	{
-		"mfussenegger/nvim-jdtls",
-		dependencies = {
-			"mfussenegger/nvim-dap",
-		},
-	},
-	{
-		"echasnovski/mini.nvim",
-		version = false,
-		config = function()
-			require("mini.files").setup()
-		end,
-	},
-	{
-		"kevinhwang91/nvim-bqf",
-	},
-	{
-		"mbbill/undotree",
-	},
-	{
-		"folke/todo-comments.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		opts = {},
-	},
-	{
-		"nanozuki/tabby.nvim",
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-	},
-	{
-		"epwalsh/obsidian.nvim",
-		version = "*",
-		lazy = true,
-		ft = "markdown",
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
-	{
-		"0xAdk/full_visual_line.nvim",
-		keys = "V",
-		opts = {},
-	},
-	{
-		"RRethy/vim-illuminate",
-	},
-	{
-		"mistweaverco/kulala.nvim",
-		keys = {
-			{ "<leader>Rs", desc = "send request" },
-			{ "<leader>Ra", desc = "send all requests" },
-			{ "<leader>Rb", desc = "open scratchpad" },
-		},
-		ft = { "http", "rest" },
-		opts = {
-			global_keymaps = true,
-			global_keymaps_prefix = "<leader>R",
-			kulala_keymaps_prefix = "",
-		},
-	},
+            -- vim.keymap.set("n", "<S-C-u>", function() require("opencode").command("session.half.page.up") end,
+            --     { desc = "Scroll opencode up" })
+            -- vim.keymap.set("n", "<S-C-d>", function() require("opencode").command("session.half.page.down") end,
+            --     { desc = "Scroll opencode down" })
+
+            -- You may want these if you use the opinionated `<C-a>` and `<C-x>` keymaps above — otherwise consider `<leader>o…` (and remove terminal mode from the `toggle` keymap)
+            vim.keymap.set("n", "+", "<C-a>", { desc = "Increment under cursor", noremap = true })
+            vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement under cursor", noremap = true })
+        end,
+    },
+    {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+            library = {
+                -- See the configuration section for more details
+                -- Load luvit types when the `vim.uv` word is found
+                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+            },
+        },
+    },
+    {
+        "kdheepak/lazygit.nvim",
+        lazy = true,
+        cmd = {
+            "LazyGit",
+            "LazyGitConfig",
+            "LazyGitCurrentFile",
+            "LazyGitFilter",
+            "LazyGitFilterCurrentFile",
+        },
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        -- setting the keybinding for LazyGit with 'keys' is recommended in
+        -- order to load the plugin when the command is run for the first time
+        keys = {
+            { "<leader>gl", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+        },
+    },
+    {
+        "karb94/neoscroll.nvim",
+        opts = {},
+    },
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
+        },
+    },
+    {
+        "akinsho/toggleterm.nvim",
+        config = true,
+    },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            "nvim-tree/nvim-web-devicons",
+        },
+    },
+    "luukvbaal/statuscol.nvim",
+    "tpope/vim-fugitive",
+    {
+        "LintaoAmons/bookmarks.nvim",
+        dependencies = {
+            { "kkharji/sqlite.lua" },
+            { "stevearc/dressing.nvim" },
+        },
+        config = function()
+            local opts = {}
+            require("bookmarks").setup(opts)
+        end,
+    },
+    {
+        "SmiteshP/nvim-navic",
+        dependencies = { "neovim/nvim-lspconfig" },
+    },
+    {
+        "ibhagwan/fzf-lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+    },
+    {
+        "saghen/blink.cmp",
+        version = "v1.8.0",
+        ---@module 'blink.cmp'
+        ---@type blink.cmp.Config
+        opts = {
+            fuzzy = { implementation = "rust" },
+            cmdline = {
+                keymap = { preset = "inherit" },
+                completion = {
+                    menu = { auto_show = true },
+                },
+            },
+            keymap = { preset = "default" },
+            completion = {
+                documentation = {
+                    window = {
+                        border = "single",
+                    },
+                },
+                menu = {
+                    -- border = { "┌", "─", "┐", "│", "┘", "─", "└", "│" },
+                    border = "single",
+                    winhighlight = "Normal:Pmenu,FloatBorder:FloatBorder,CursorLine:PmenuSel,Search:Search",
+                    draw = {
+                        columns = { { "kind_icon" }, { "label", "label_description", gap = 1 }, { "kind" } },
+                        components = {
+                            label = {
+                                width = { fill = true, max = 120 },
+                            },
+                            label_description = {
+                                width = { fill = true, max = 120 },
+                            },
+                            kind = {
+                                text = function(ctx)
+                                    return "[" .. ctx.kind .. "]"
+                                end,
+                            },
+                        },
+                    },
+                },
+            },
+            signature = {
+                window = {
+                    border = "single",
+                },
+            },
+            appearance = {
+                use_nvim_cmp_as_default = true,
+                nerd_font_variant = "normal",
+            },
+            sources = {
+                default = { "lsp", "path", "snippets", "buffer", "dadbod", "lazydev" },
+                providers = {
+                    lazydev = {
+                        name = "LazyDev",
+                        module = "lazydev.integrations.blink",
+                        score_offset = 100,
+                    },
+                    dadbod = { name = "Dadbod", module = "vim_dadbod_completion.blink" },
+                },
+                min_keyword_length = function(ctx)
+                    return ctx.trigger.kind == "trigger_character" and 0 or 3
+                end,
+            },
+        },
+        opts_extend = { "sources.default" },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = { "saghen/blink.cmp" },
+
+        -- example using `opts` for defining servers
+        opts = {
+            servers = {},
+        },
+        config = function(_, opts)
+            local lspconfig = require("lspconfig")
+            for server, config in pairs(opts.servers) do
+                -- passing config.capabilities to blink.cmp merges with the capabilities in your
+                -- `opts[server].capabilities, if you've defined it
+                config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+                lspconfig[server].setup(config)
+            end
+        end,
+
+        -- example calling setup directly for each LSP
+        -- config = function()
+        -- 	local capabilities = require("blink.cmp").get_lsp_capabilities()
+        -- 	-- local lspconfig = require("lspconfig")
+        --
+        -- 	vim.lsp.config["lua-ls"].setup({ capabilities = capabilities })
+        -- end,
+    },
+    {
+        "stevearc/conform.nvim",
+        opts = {},
+    },
+    {
+
+        "L3MON4D3/LuaSnip",
+        dependencies = { "rafamadriz/friendly-snippets" },
+    },
+    {
+        "folke/which-key.nvim",
+    },
+
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        opts = {},
+    },
+    {
+        "windwp/nvim-ts-autotag",
+        config = function() end,
+    },
+    {
+        "mfussenegger/nvim-jdtls",
+        dependencies = {
+            "mfussenegger/nvim-dap",
+        },
+    },
+    {
+        "echasnovski/mini.nvim",
+        version = false,
+        config = function()
+            require("mini.files").setup()
+        end,
+    },
+    {
+        "kevinhwang91/nvim-bqf",
+    },
+    {
+        "mbbill/undotree",
+    },
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {},
+    },
+    {
+        "nanozuki/tabby.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+    },
+    {
+        "epwalsh/obsidian.nvim",
+        version = "*",
+        lazy = true,
+        ft = "markdown",
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
+    {
+        "0xAdk/full_visual_line.nvim",
+        keys = "V",
+        opts = {},
+    },
+    {
+        "RRethy/vim-illuminate",
+    },
+    {
+        "mistweaverco/kulala.nvim",
+        keys = {
+            { "<leader>Rs", desc = "send request" },
+            { "<leader>Ra", desc = "send all requests" },
+            { "<leader>Rb", desc = "open scratchpad" },
+        },
+        ft = { "http", "rest" },
+        opts = {
+            global_keymaps = true,
+            global_keymaps_prefix = "<leader>R",
+            kulala_keymaps_prefix = "",
+        },
+    },
 }
