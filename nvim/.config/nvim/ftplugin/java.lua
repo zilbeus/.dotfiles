1bd81d990ed3f9a55f2cdc9cab4134d785f5c795
zilbeus <118920399+zilbeus@users.noreply.github.com>
2026-04-15 00:04:37 +0300

    update nvim java config, add debug plugins

--- nvim/.config/nvim/ftplugin/java.lua
+++ nvim/.config/nvim/ftplugin/java.lua
@@ -57,27 +57,60 @@ local config = {
 						name = "JavaSE-17",
 						path = "/usr/lib/jvm/java-17-openjdk-17.0.12.0.7-2.fc40.x86_64",
 					},
+					{
+						name = "JavaSE-21",
+						path = "/usr/lib/jvm/java-21-openjdk",
+					},
+					{
+						name = "JavaSE-25",
+						path = "/usr/lib/jvm/java-25-openjdk",
+					},
 				},
 			},
 		},
 	},
+}
 
-	-- Language server `initializationOptions`
-	-- You need to extend the `bundles` with paths to jar files
-	-- if you want to use additional eclipse.jdt.ls plugins.
-	--
-	-- See https://github.com/mfussenegger/nvim-jdtls#java-debug-installation
-	--
-	-- If you don't plan on using the debugger or other eclipse.jdt.ls plugins you can remove this
-	init_options = {
-		bundles = {
-			vim.fn.glob(
-				vim.fn.stdpath("data")
-					.. "/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar",
-				true
-			),
-		},
-	},
+local bundles = {}
+
+local java_test_bundles = vim.split(
+	vim.fn.glob(vim.fn.stdpath("data") .. "/mason/packages/java-test/extension/server/*.jar", true),
+	"\n",
+	{ trimempty = true }
+)
+
+local excluded = {
+	"com.microsoft.java.test.runner-jar-with-dependencies.jar",
+	"jacocoagent.jar",
+}
+
+local java_test_plugin_jar = nil
+
+for _, java_test_jar in ipairs(java_test_bundles) do
+	local fname = vim.fn.fnamemodify(java_test_jar, ":t")
+	if vim.startswith(fname, "com.microsoft.java.test.plugin-") then
+		java_test_plugin_jar = java_test_jar
+	elseif not vim.tbl_contains(excluded, fname) then
+		table.insert(bundles, java_test_jar)
+	end
+end
+
+if java_test_plugin_jar then
+	table.insert(bundles, java_test_plugin_jar)
+end
+
+local java_debug_jar = vim.fn.glob(
+	vim.fn.stdpath("data")
+		.. "/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar",
+	true
+)
+
+if java_debug_jar ~= "" then
+	table.insert(bundles, java_debug_jar)
+end
+
+config["init_options"] = {
+	bundles = bundles,
 }
 
 require("jdtls").start_or_attach(config)
